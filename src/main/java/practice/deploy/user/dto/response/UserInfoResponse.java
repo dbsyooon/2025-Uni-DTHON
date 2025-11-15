6dab72064f754b55b28ad2c2018ec611995ac9c2
erika0915 <soohee2001@gmail.com>
2025-11-16 08:23:08 +0900

    feat: sleepTime 수정

--- src/main/java/practice/deploy/user/dto/response/UserInfoResponse.java
+++ src/main/java/practice/deploy/user/dto/response/UserInfoResponse.java
@@ -1,5 +1,6 @@
 package practice.deploy.user.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import practice.deploy.user.domain.User;
 import practice.deploy.user.domain.type.Gender;
 
@@ -9,6 +10,7 @@ import java.time.LocalTime;
 public record UserInfoResponse(
         Gender gender,
         Long age,
+        @JsonFormat(pattern = "HH:mm")
         LocalTime sleepTime
 ) {
     public static UserInfoResponse from(User user){
